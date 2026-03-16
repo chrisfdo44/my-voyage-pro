9f9f18404a154d176bf51c5382c5ba7e91edb3cb
Christopher Fernando <christopherfdo44@gmail.com>
2026-03-16 22:50:09 +0400

    Add useRef import to VesselCargoCalculator component

--- src/components/calculators/VesselCargoCalculator.tsx
+++ src/components/calculators/VesselCargoCalculator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { fetchBackendFromWebApp, type CargoRow, type VesselRow } from "../../lib/sheetsApi";
 import html2canvas from "html2canvas";
 
